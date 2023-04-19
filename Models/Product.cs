e12d202e60177c75fdebc8e9e13d2c765e39b0a2
André Desiderio <andrymetal21@gmail.com>
2023-04-19 22:13:12 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GroceryList.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        [Required]
+        public int Quantity { get; set; }
+
+        [Range(1, 100)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GroceryList.Data;
+using System;
+using System.Linq;
+
+namespace GroceryList.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new GroceryListContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<GroceryListContext>>()))
+            {
+                // Look for any Products.
+                if (context.Product.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Product.AddRange(
+                    new Product
+                    {
+                        Name = "Eggs",
+                        Quantity= 1,
+                        Price= 3,
+
+                    },
+
+                    new Product
+                    {
+                        Name = "Bread",
+                        Quantity = 1,
+                        Price = 2,
+                    },
+
+                    new Product
+                    {
+                        Name = "meat",
+                        Quantity = 2,
+                        Price = 3,
+                    },
+
+                    new Product
+                    {
+                        Name = "Peanut Butter",
+                        Quantity = 1,
+                        Price = 5,
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
